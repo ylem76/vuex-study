e79858e6e34bce50231698152f8609903762ceb0
ylem76 <ylem76@thebreeze.co.kr>
2020-11-30 17:56:24 +0900

    mapGetters와 mapActions

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -1,13 +1,22 @@
 <template>
-  <button @click="addOne">add 2</button>
+  <button @click="increaseCall">increment add</button>
+  <button @click="increase({ value: 11 })">increase add</button>
 </template>
 <script>
+import { mapActions } from 'vuex';
 export default {
   methods: {
-    addOne() {
-      //this.$store.commit('increment');
-      this.$store.dispatch('increment');
-    }
+    // addOne() {
+    //   //this.$store.commit('increment');
+    //   this.$store.dispatch('increment');
+    // }
+    // ...mapActions(['increment', 'increase'])
+    // 액션과 부르는 키가 같을 때
+    ...mapActions({
+      //액션과 부르는 키를 다르게 설정할 때
+      increaseCall: 'increment', //template에서 increaseCall / 실제 action에서 increment
+      increase: 'increase' //template에서 increase / 실제 action에서 increase
+    })
   }
 };
 </script>
--- src/components/TheCounter.vue
+++ src/components/TheCounter.vue
@@ -1,15 +1,20 @@
 <template>
-  <h3>{{ counter }}</h3>
+  <h3>{{ finalCounter }}</h3>
 </template>
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   computed: {
-    counter() {
-      return this.$store.getters.finalCounter;
-      // return this.$store.getters.finalCounter(); 함수를 실행할 필요가 없다. 그저 가리키기만 하면 된다.
-      // 그렇게 하면 vuex가 알아서 getter 메소드를 실행할 것이다.
-      // 실행하면서 현재의 state상태를 getter로 넘기게 될 것이고, getter에서 그것을 계산할 것이다.
-    }
+    //counter() {
+    //return this.$store.getters.finalCounter;
+    // return this.$store.getters.finalCounter(); 함수를 실행할 필요가 없다. 그저 가리키기만 하면 된다.
+    // 그렇게 하면 vuex가 알아서 getter 메소드를 실행할 것이다.
+    // 실행하면서 현재의 state상태를 getter로 넘기게 될 것이고, getter에서 그것을 계산할 것이다.
+    //}
+    ...mapGetters(['finalCounter'])
+    //mapGetters 여러가지 getters를 불러오는 코드, 여러 개의 getters를 불러올 수 있다.
+    //이대로 적으면 이전 코드와 동일하게 동작한다. 대신, finalCounters를 mapGetters로 불러온 모습
   }
 };
 </script>
